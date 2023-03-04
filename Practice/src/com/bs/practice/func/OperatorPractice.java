4c091a0553be2c8093bbf45cdf1028ac0a54be73
Inho Choi <inhoru126@gmail.com>
2023-03-05 02:09:03 +0900

    연산자 실습1

---
+++ Practice/src/com/bs/practice/func/OperatorPractice.java
@@ -0,0 +1,13 @@
+package com.bs.practice.func;
+
+import java.util.Scanner;
+
+public class OperatorPractice {
+	public void practice1() {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("정수 :");
+		int num = sc.nextInt();
+		System.out.println(num>0?"양수다":"양수가 아니다");
+		
+	}
+}
---
+++ Practice/src/com/bs/practice/run/Run.java
@@ -0,0 +1,13 @@
+package com.bs.practice.run;
+
+import com.bs.practice.func.OperatorPractice;
+
+public class Run {
+	public static void main(String[] args) {
+		new OperatorPractice().practice1();
+		
+		
+
+	}
+
+}
