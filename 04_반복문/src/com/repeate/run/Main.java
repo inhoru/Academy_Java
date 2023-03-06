23f577bfd2a565279967b332eff996664d672177
Inho Choi <inhoru126@gmail.com>
2023-03-06 21:03:46 +0900

    03-06

---
+++ 04_반복문/src/com/repeate/run/Main.java
@@ -0,0 +1,9 @@
+package com.repeate.run;
+
+import com.repeate.controller.RepeatController;
+
+public class Main {
+	public static void main(String[] args) {
+		new RepeatController().basicRepeat();
+	}
+}
