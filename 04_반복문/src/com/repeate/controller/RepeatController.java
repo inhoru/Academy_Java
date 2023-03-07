9bbe7e8d2688f2fbbc9865c29dcdb79e2921eb7e
Inho Choi <inhoru126@gmail.com>
2023-03-07 22:07:36 +0900

    가위바위보 완성

--- 04_반복문/src/com/repeate/controller/RepeatController.java
+++ 04_반복문/src/com/repeate/controller/RepeatController.java
@@ -460,6 +460,9 @@ public class RepeatController {
 		int rad = 0;
 		int su;
 		char choice = ' ';
+		int win = 0;
+		int lose = 0;
+		int draw = 0;
 		do {
 			System.out.println("===가위바위보게임===");
 			System.out.println("1. 가위");
@@ -474,11 +477,14 @@ public class RepeatController {
 			case 1:
 				if (su == rad) {
 					System.out.println("비겼습니다.");
+					draw++;
 				} else if (2 == rad) {
 					System.out.println("컴퓨터가 이겼습니다.");
+					lose++;
 					choice = ' ';
 				} else if (3 == rad) {
 					System.out.println("사용자가 이겼습니다.");
+					win++;
 					choice = ' ';
 				}
 				break;
@@ -486,23 +492,30 @@ public class RepeatController {
 			case 2:
 				if (su == rad) {
 					System.out.println("비겼습니다.");
+					draw++;
 				} else if (1 == rad) {
 					System.out.println("사용자가 이겼습니다.");
+					win++;
 					choice = ' ';
 				} else if (3 == rad) {
 					System.out.println("컴퓨터가 이겼습니다.");
+					lose++;
 					choice = ' ';
+					
 				}
 				break;
 
 			case 3:
 				if (su == rad) {
 					System.out.println("비겼습니다.");
+					draw++;
 				} else if (1 == rad) {
 					System.out.println("컴퓨터가 이겼습니다.");
+					win++;
 					choice = ' ';
 				} else if (2 == rad) {
 					System.out.println("사용자가 이겼습니다.");
+					lose++;
 					choice = ' ';
 				}
 				break;
@@ -516,7 +529,8 @@ public class RepeatController {
 			}
 		} while (su == rad || (choice == 'Y'));
 
-		System.out.println("프로그램을 종료하겠습니다!");
+		System.out.println("프로그램을 종료하겠습니다! 전적은 : " + win + "승 " + lose + "패 "
+				+ draw + "무 입니다.");
 	}
 	
 
