6d13bc054c85572a192e8fb47fd837f0ea083ac5
inhoru <inhoru126@gmail.com>
2023-03-07 11:02:04 +0900

    03-07

--- 04_반복문/src/com/repeate/run/Main.java
+++ 04_반복문/src/com/repeate/run/Main.java
@@ -4,6 +4,10 @@ import com.repeate.controller.RepeatController;
 
 public class Main {
 	public static void main(String[] args) {
-		new RepeatController().basicRepeat();
+		//new RepeatController().basicRepeat();
+		//new RepeatController().forApplication();
+		//new RepeatController().empInputData();
+		//new RepeatController().forApplication2();
+		new RepeatController().forInfor();
 	}
 }
