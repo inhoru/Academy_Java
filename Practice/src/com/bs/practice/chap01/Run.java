45d69c469370e687b5f9be93acdc5ab8c7ad5911
Inho Choi <inhoru126@gmail.com>
2023-03-08 01:11:16 +0900

    조건문 실습문제완료

--- Practice/src/com/bs/practice/chap01/Run.java
+++ Practice/src/com/bs/practice/chap01/Run.java
@@ -6,7 +6,15 @@ public class Run {
 
 	public static void main(String[] args) {
 		//new ControlPractice().practice1();
-		new ControlPractice().practice2();
+		//new ControlPractice().practice2();
+		//new ControlPractice().practice3();
+		//new ControlPractice().practice4();
+		//new ControlPractice().practice5();
+		//new ControlPractice().practice6();
+		//new ControlPractice().practice7();
+		//new ControlPractice().practice9();
+		new ControlPractice().practice10();
+		
 
 	}
 
