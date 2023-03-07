0525466be2f98cd32345580e94864a4da9e41f8a
Inho Choi <inhoru126@gmail.com>
2023-03-07 20:04:45 +0900

    Delete 05_배열 directory

--- 05_배열/src/com/array/run/Main.java
+++
@@ -1,9 +0,0 @@
-package com.array.run;
-
-import com.array.controller.ArrayController;
-
-public class Main {
-	public static void main(String[] args) {
-		new ArrayController().basicArray();
-	}
-}
