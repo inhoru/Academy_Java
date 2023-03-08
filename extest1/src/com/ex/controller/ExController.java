51bd090a15d6a43c87933257acd82ae00982a7a9
inhoru <inhoru126@gmail.com>
2023-03-08 18:57:34 +0900

    프로그래밍언어시험

--- extest1/src/com/ex/controller/ExController.java
+++ extest1/src/com/ex/controller/ExController.java
@@ -43,9 +43,8 @@ public class ExController {
 		int num = sc.nextInt();
 		System.out.print("초콜릿 개수 : ");
 		int num1 = sc.nextInt();
-		double total = (double) num1 / num;
-		double rest = (num % num1);
-		System.out.print("1인당 나눠가진 초콜릿개수 : " + (total) + "\n나눠주고 남은 초콜릿개수 : " + rest);
+		System.out.print("1인당 나눠가진 초콜릿개수 : " + (num1/num) +
+				"\n나눠주고 남은 초콜릿개수 : " + (num1%num));
 
 	}
 
@@ -57,42 +56,56 @@ public class ExController {
 		int score1 = sc.nextInt();
 		System.out.print("수학점수 : ");
 		int score2 = sc.nextInt();
-		int total = score + score1 + score2;
-		System.out.println("합계 : " + total + "\n평균 : " + total / 3.0);
+		int total = (score + score1 + score2);
+		double avg = (score+score1+score2)/3.0;
+		System.out.println("합계 : " + total + "\n평균 : " + avg);
+		System.out.println(score>=40&&score1>=40&&score2>=40?"합격":"불합격");
 	}
 
 	public void ex6() {
 		Scanner sc = new Scanner(System.in);
-		System.out.print("중간고사 : ");
-		int score11 = sc.nextInt();
-		System.out.print("기말고사 : ");
-		int score111 = sc.nextInt();
-		System.out.print("과제점수 : ");
-		int score21 = sc.nextInt();
-		System.out.print("출석회수 : ");
-		int score3 = sc.nextInt();
-		int total1 = (score111 + score111 + score21 + score3);
-		double averge = total1 / 4.0;
-		if (score111 >= 40 && score111 >= 40 && score21 >= 40 && score3 >= 40 && total1 >= 160 && averge >= 60) {
-			System.out.println("합격");
-		} else {
-			System.out.println("불합격");
+		System.out.print("중간 고사 점수 : ");
+		int score = sc.nextInt();
+		System.out.print("기말 고사 점수 : ");
+		int score1 = sc.nextInt();
+		System.out.print("과제 점수 : ");
+		int tast = sc.nextInt();
+		System.out.print("출석 회수 : ");
+		int at = sc.nextInt();
+		double average = (score * 20.0) / 100.0;
+		double average1 = (score1 * 30.0) / 100.0;
+		double average2 = (tast * 30.0) / 100.0;
+		double average3 = (at * 20.0) / 20.0;
+		double total = average + average1 + average2 + average3;
+
+		System.out.println("=========결과=========");
+		if (70 <= total && at >= 14) {
+			System.out.print("중간 고사 점수(20) : " + average + "\n기말 고사 점수(30) : " + average1 + "\n과제점수\t(30) : " + average2
+					+ "\n출석점수\t(20) : " + average3 + "\n총점 : " + total + "\nPASS\n");
+		} else if (70 > total && at >= 14) {
+			System.out.print("중간 고사 점수(20) : " + average + "\n기말 고사 점수(30) : " + average1 + "\n과제점수\t(30) : " + average2
+					+ "\n출석점수\t(20) : " + average3 + "\n총점 : " + total + "\nFail [점수 미달]\n");
+		} else if (at <= 14) {
+			System.out.println("Fall [출석 회수 부족]" + (at + "/20\n"));
+
 		}
 	}
 
 	public void ex7() {
-
-		for (int i = 0; i < 4; i++) {
-			for (int j = 0; j < i; j++) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("정수입력 : ");
+		int su =sc.nextInt();
+		for (int i = 0; i < su;i++) {  // i가 0이고 su를4로작성하면 0 1 2 3 <- 총4번반복
+			for (int j = 0; j < i+1; j++) { //i가0 참이되고 바로내려와서 j가0부터다시시작 반복
 				System.out.print("*");
 			}
-			System.out.println("*");
+			System.out.println();
 		}
 	}
 
 	public void ex8() {
 		Scanner sc = new Scanner(System.in);
-		int su = 0;
+
 		do {
 			System.out.println("====프로그램실행====");
 			System.out.println("1.계산기");
@@ -104,83 +117,37 @@ public class ExController {
 			System.out.println("7.별찍기");
 			System.out.println("9.프로그램종료");
 			System.out.print("입력 : ");
-			su = sc.nextInt();
+			int su = sc.nextInt();
 			switch (su) {
 			case 1:
-				System.out.print("정수 입력 : ");
-				int num = sc.nextInt();
-				System.out.print("정수 입력 : ");
-				int num1 = sc.nextInt();
-
-				System.out.println("합 : " + (num + num1));
-				System.out.println("차 : " + (num - num1));
-				System.out.println("곱 : " + (num * num1));
-				System.out.println("나누기 : " + ((double) num / num1));
+				ex1();
 				break;
 			case 2:
-				System.out.print("문자 입력 : ");
-				char ch = sc.next().charAt(0);
-
-				System.out.println((int) ch);
+				ex2();
 				break;
 			case 3:
-				System.out.print("영어문자열 입력 : ");
-				sc.nextLine();
-				String msg = sc.nextLine();
-				char ch1 = msg.charAt(0);
-				char ch2 = msg.charAt(1);
-				char ch3 = msg.charAt(2);
-				System.out.println("" + ch1 + "" + "" + ch2 + "" + "" + ch3 + "");
+				ex3();
 				break;
 
 			case 4:
-				System.out.print("인원 수 : ");
-				int people = sc.nextInt();
-				System.out.print("초콜릿 개수 : ");
-				int chocolate = sc.nextInt();
-				double total = (double) chocolate / people;
-				double rest = (people % chocolate);
-				System.out.println("1인당 나눠가진 초콜릿개수 : " + (total) + "\n나눠주고 남은 초콜릿개수 : " + rest + "\n");
+				ex4();
 				break;
 			case 5:
-				System.out.print("국어점수 : ");
-				int score = sc.nextInt();
-				System.out.print("영어점수 : ");
-				int score1 = sc.nextInt();
-				System.out.print("수학점수 : ");
-				int score2 = sc.nextInt();
-				int totall = score + score1 + score2;
-				System.out.println("합계 : " + totall + "\n평균 : " + totall / 3.0);
+				ex5();
 				break;
 			case 6:
-				System.out.print("중간고사 : ");
-				int score11 = sc.nextInt();
-				System.out.print("기말고사 : ");
-				int score111 = sc.nextInt();
-				System.out.print("과제점수 : ");
-				int score21 = sc.nextInt();
-				System.out.print("출석회수 : ");
-				int score3 = sc.nextInt();
-				int total1 = (score111 + score111 + score21 + score3);
-				double averge = total1 / 4.0;
-				if (score111 >= 40 && score111 >= 40 && score21 >= 40 && score3 >= 40 && total1 >= 160 && averge >= 60) {
-					System.out.println("합격");
-				} else {
-					System.out.println("불합격");
-				}break;
+				ex6();
+				break;
 			case 7:
-				for (int i = 0; i < 4; i++) {
-					for (int j = 0; j <i; j++) {
-						System.out.print("*");
-					}
-					System.out.println("*");
-				}break;
-			
-			case 9:
+				ex7();
+				break;
 
+			}
+			if (su == 9) {
 				System.out.println("프로그램을종료합니다.");
 				break;
 			}
-		} while (su != 9);
+
+		} while (true);
 	}
 }
--- extest1/src/com/ex/run/Main.java
+++ extest1/src/com/ex/run/Main.java
@@ -9,11 +9,11 @@ public class Main {
 		//new ExController().ex1();
 		//new ExController().ex2();
 		//new ExController().ex3();
-		//new ExController().ex4();
+		new ExController().ex4();
 		//new ExController().ex5();
 		//new ExController().ex6();
 		//new ExController().ex7();
-		new ExController().ex8();
+		//new ExController().ex8();
 	}
 
 }
