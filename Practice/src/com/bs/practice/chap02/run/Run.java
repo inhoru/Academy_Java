258d770a72abd3bee749a5174f47981c990a5d1b
inhoru <inhoru126@gmail.com>
2023-03-09 09:08:55 +0900

    Merge remote-tracking branch 'origin/main' into main

--- Practice/src/com/bs/practice/chap02/run/Run.java
+++ Practice/src/com/bs/practice/chap02/run/Run.java
@@ -16,7 +16,13 @@ public class Run {
 		//new LoopPractice().practice9();
 		//new LoopPractice().practice10();
 		//new LoopPractice().practice11();
-		new LoopPractice().practice12();
+		//new LoopPractice().practice12();
+		//new LoopPractice().practice13();
+		//new LoopPractice().practice14();
+		//new LoopPractice().practice15();
+		//new LoopPractice().practice16();
+		//new LoopPractice().practice17();
+		new LoopPractice().practice18();
 
 	}
 
