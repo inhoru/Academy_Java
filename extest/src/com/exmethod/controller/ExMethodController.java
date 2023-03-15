a45054c9d44b667ddaca39383677e65070853d0b
inhoru <inhoru126@gmail.com>
2023-03-15 17:52:02 +0900

    객체 실습문제

---
+++ extest/src/com/exmethod/controller/ExMethodController.java
@@ -0,0 +1,19 @@
+package com.exmethod.controller;
+
+import java.util.Scanner;
+
+public class ExMethodController {
+	
+	public void msg (String msg) {
+		System.out.println(msg);
+		
+	}
+	public void sum(int sum) {
+		System.out.println(sum);
+	}
+	public boolean oddeven() {
+		return 
+		
+		
+	}
+}
---
+++ extest/src/com/exmethod/run/Main.java
@@ -0,0 +1,27 @@
+package com.exmethod.run;
+
+import com.exmethod.controller.ExMethodController;
+
+public class Main {
+
+	public static void main(String[] args) {
+		ExMethodController  e = new ExMethodController();
+		e.msg("메세지 전달");
+		
+		int sum = 0;
+		for(int i = 1;i<=100;i++) {
+			sum+=i;
+		}
+		e.sum(sum);
+		
+		
+		if(
+		
+		e.oddeven(true);{
+			
+		}
+		
+
+	}
+
+}
