58465c316d763da5f35f11105a75fa157eca6d5e
GDC10 <GDC10@DESKTOP-S7S1U7J>
2023-04-19 17:14:46 +0900

    JDBC

---
+++ jdbcTest2/jdbcTest2/src/com/jdbc/controller/MemberTestController.java
@@ -0,0 +1,45 @@
+package com.jdbc.controller;
+
+import com.jdbc.common.MemberController;
+
+public class MemberTestController implements MemberController{
+
+	@Override
+	public void selectMemberById() {
+		// TODO Auto-generated method stub
+		System.out.println("아이디로 회원조회");
+	}
+
+	@Override
+	public void selectAllMember() {
+		// TODO Auto-generated method stub
+		System.out.println("전체회원조회");
+	}
+
+	@Override
+	public void insertMember() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void updateMember() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void selectMemberByName() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mainMenu() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+	
+}
